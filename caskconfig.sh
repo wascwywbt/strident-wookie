99c6565f819aab1f48c22a01ef66a20d88e718ca
wascwywbt <312d4414@opayq.com>
2017-04-17 19:35:54 -0400

    Add files via upload

---
+++ caskconfig.sh
@@ -0,0 +1,81 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf820
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\csgray\c0;\csgray\c100000;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #!/bin/sh\
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f1\fs22 \cf2 \cb3 \CocoaLigature0 brew cask install 1password\
+brew cask install adobe-acrobat\
+brew cask install adobe-photoshop-lightroom\
+brew cask install alfred\
+brew cask install amazon-drive\
+brew cask install appcleaner\
+brew cask install atext\
+brew cask install bartender\
+brew cask install caffeine\
+brew cask install calibre\
+brew cask install camtasia\
+brew cask install carbon-copy-cloner\
+brew cask install cheatsheet\
+brew cask install chrome-remote-desktop-host\
+brew cask install mkchromecast\
+brew cask install cloudup\
+brew cask install crashplan\
+brew cask install daisydisk\
+brew cask install dayone-cli\
+brew cask install diskmaker-x\
+brew cask install droidid\
+brew cask install dropbox\
+brew cask install droplr\
+brew cask install duet\
+brew cask install evernote\
+brew cask install fantastical\
+brew cask install firefox\
+brew cask install titbit-connect\
+brew cask install gemini\
+brew cask install google-drive\
+brew cask install google-earth\
+brew cask install google-earth-pro\
+brew cask install google-photos-backup\
+brew cask install grammarly\
+brew cask install i1profiler\
+brew cask install hazel\
+brew cask install lastpass\
+brew cask install logitech-options\
+brew cask install logmein-hamachi\
+brew cask install malwarebytes-anti-malware\
+brew cask install microsoft-office\
+brew cask install mightytext\
+brew cask install omnidisksweeper\
+brew cask install omnifocus\
+brew cask install overdrive-media-console\
+brew cask install parallels-desktop\
+brew cask install pdfpenpro\
+brew cask install paragon-ntfs\
+brew cask install private-internet-access\
+brew cask install screens-connect\
+brew cask install simplenote\
+brew cask install skitch\
+brew cask install skype\
+brew cask install slack\
+brew cask install sling\
+brew cask install snagit\
+brew cask install spotify\
+brew cask install synology-cloud-station-backup\
+brew cask install synology-cloud-station-drive\
+brew cask install synology-photo-station-uploader\
+brew cask install teamviewer\
+brew cask install tunnelblick\
+brew cask install viber\
+brew cask install vlc\
+brew cask install vox\
+brew cask install WhatsApp\
+brew cask install wordpresscom\
+brew cask install yandex\
+brew cask install zoomus\
+\
+}
