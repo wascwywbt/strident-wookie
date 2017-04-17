be8c1cfb638a10899144a165befcb3406ee2fa66
wascwywbt <312d4414@opayq.com>
2017-04-17 19:37:39 -0400

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -1,14 +1,5 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf820
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;}
-{\*\expandedcolortbl;;\csgray\c0;\csgray\c100000;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 #!/bin/sh\
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
-
-\f1\fs22 \cf2 \cb3 \CocoaLigature0 brew cask install 1password\
+#!/bin/sh
+brew cask install 1password\
 brew cask install adobe-acrobat\
 brew cask install adobe-photoshop-lightroom\
 brew cask install alfred\
@@ -77,5 +68,3 @@ brew cask install WhatsApp\
 brew cask install wordpresscom\
 brew cask install yandex\
 brew cask install zoomus\
-\
-}
